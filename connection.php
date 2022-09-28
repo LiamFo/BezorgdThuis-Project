7f4d4f7fb4da834f5a7848c2a7cb015482da7511
Erkan2 <E.aslantas@hotmail.com>
2022-09-28 22:29:07 +0200

    push changes

---
+++ connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$sname= "localhost";
+$unmae= "id19625817_erkanlaimmike";
+$password = "v5U6TPU#|lD?[-Hp";
+$db_name = "id19625817_project_bezorgthuis";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if ($conn) {
+	echo "Connection succus";
+
+    if (!$conn) {
+        echo "Connection failed!";
+    }
+}
