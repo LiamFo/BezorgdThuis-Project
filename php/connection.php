52316a095811c3dca70b69d817ab8453976cad3f
Erkan21211 <82599320+Erkan21211@users.noreply.github.com>
2022-10-23 12:58:39 +0200

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,9 +1,9 @@
 <?php
 
 $sname= "localhost";
-$unmae= "id19625817_erkanlaimmike";
-$password = "v5U6TPU#|lD?[-Hp";
-$db_name = "id19625817_project_bezorgthuis";
+$unmae= "";
+$password = "";
+$db_name = "";
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
